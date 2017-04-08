7c25fbb4f6dc84295924348b265d1ec60827a5ea
marian.faryna <marian.faryna@vitech.com.ua>
2017-04-08 19:17:42 +0300

    added last match id to start querying from specific match if provided

--- rest-client/src/main/scala/com/dota/rest/client/Main.scala
+++ rest-client/src/main/scala/com/dota/rest/client/Main.scala
@@ -5,43 +5,52 @@ class Main {}
 object Main extends App {
  val client = RestClient
 
-//  val restResponse = client.getMatches()
-//  println("=======> REST RESPONSE")
-//  println(restResponse)
+  for( i <- 1 to 10) {
 
-// for(playedMatch <- restResponse.matches) {
-//
-//
-//   val matchDetails = client.getMatchHistory(playedMatch.matchId)
-//   println("=======> REST RESPONSE")
-//   println(matchDetails.matchId)
-//   println(matchDetails.gameMode)
-//   println("=======> KILLS")
-//   println(matchDetails.players.head.kills)
-//   println("=======> DEATHS")
-//   println(matchDetails.players.head.deaths)
-//   println("=======> GOLD")
-//   println(matchDetails.players.head.goldCurrent)
-//   println(matchDetails.players.head.goldSpent)
-//   println(matchDetails.players.head.goldPerMinute)
-//   println(matchDetails.players.head.goldPerMinute)
-//   println("=======> DAMAGE")
-//   println(matchDetails.players.head.heroDamage)
-//   println(matchDetails.players.head.heroHealing)
-//   println(matchDetails.players.head.xpPerMinute)
-//   println("=======> ITEMS")
-//   println(matchDetails.players.head.itemOne)
-//   println(matchDetails.players.head.itemTwo)
-//   println(matchDetails.players.head.itemThree)
-//   println(matchDetails.players.head.itemFour)
-//   println(matchDetails.players.head.itemFive)
-//   println("=======> ABILITIES")
-//   if(matchDetails.players.head.abilityUpgrades.nonEmpty) {
-//     println(matchDetails.players.head.abilityUpgrades.head.ability)
-//     println(matchDetails.players.head.abilityUpgrades.head.level)
-//     println(matchDetails.players.head.abilityUpgrades.head.time)
-//   }
-// }
+    val restResponse = client.getMatches
+//    println("=======> REST RESPONSE")
+//    println(restResponse)
+
+        println("=======> MATCHES SIZE : " + restResponse.matches.length)
+
+    var latestId: String = ""
+    for (playedMatch <- restResponse.matches) {
+
+
+//      val matchDetails = client.getMatchHistory(playedMatch.matchId)
+//      println("=======> REST RESPONSE")
+//      println(matchDetails.matchId)
+//      println(matchDetails.gameMode)
+//      println("=======> KILLS")
+//      println(matchDetails.players.head.kills)
+//      println("=======> DEATHS")
+//      println(matchDetails.players.head.deaths)
+//      println("=======> GOLD")
+//      println(matchDetails.players.head.goldCurrent)
+//      println(matchDetails.players.head.goldSpent)
+//      println(matchDetails.players.head.goldPerMinute)
+//      println(matchDetails.players.head.goldPerMinute)
+//      println("=======> DAMAGE")
+//      println(matchDetails.players.head.heroDamage)
+//      println(matchDetails.players.head.heroHealing)
+//      println(matchDetails.players.head.xpPerMinute)
+//      println("=======> ITEMS")
+//      println(matchDetails.players.head.itemOne)
+//      println(matchDetails.players.head.itemTwo)
+//      println(matchDetails.players.head.itemThree)
+//      println(matchDetails.players.head.itemFour)
+//      println(matchDetails.players.head.itemFive)
+//      println("=======> ABILITIES")
+//      if (matchDetails.players.head.abilityUpgrades.nonEmpty) {
+//        println(matchDetails.players.head.abilityUpgrades.head.ability)
+//        println(matchDetails.players.head.abilityUpgrades.head.level)
+//        println(matchDetails.players.head.abilityUpgrades.head.time)
+//      }
+      println("=======> MATCH ID " + playedMatch.matchId)
+      latestId = String.valueOf(playedMatch.matchId)
+    }
+    client.latestMatchId = latestId
+  }
 //  println("=======> ITEMS")
 //  val restItemsResponse = client.getGameItems
 //    println("=======> REST RESPONSE")
@@ -64,12 +73,12 @@ object Main extends App {
 //    println(hero.name)
 //  }
 
-  println("=======> ABILITIES")
-  val restAbilitiesResponse = client.getHeroAbilities
-  println("=======> REST RESPONSE")
-  for(ability <- restAbilitiesResponse) {
-    println("=======> ITEM")
-    println(ability.id)
-    println(ability.name)
-  }
+//  println("=======> ABILITIES")
+//  val restAbilitiesResponse = client.getHeroAbilities
+//  println("=======> REST RESPONSE")
+//  for(ability <- restAbilitiesResponse) {
+//    println("=======> ITEM")
+//    println(ability.id)
+//    println(ability.name)
+//  }
 }
