cd76d118bf131982c6f5bd4c85599541fb526fb0
marian.faryna <marian.faryna@vitech.com.ua>
2017-04-08 19:17:42 +0300

    added last match id to start querying from specific match if provided

--- rest-client/src/main/scala/com/dota/rest/client/RestClient.scala
+++ rest-client/src/main/scala/com/dota/rest/client/RestClient.scala
@@ -8,19 +8,21 @@ import com.dota.rest.entity.items.{Item, ItemsResponse}
 import com.dota.rest.entity.matches.{Match, Response, Result, _}
 import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.apache.commons.lang3.StringUtils
 
 class RestClient {
 
 }
 
 object RestClient {
-  val latestMatchId :String = ""
+  var latestMatchId :String = "" //start_at_match_id=${match_id}
 
   val DOTA_KEY = "34DE638F08E3997DEF092DF09C1C5090"
   val START_AT_MATCH_ID = "?start_at_match_id="
   val DOTA_BASIC_REST_URL = "https://api.steampowered.com/IDOTA2Match_570/"
   val DOTA_ECON_BASIC_REST_URL = "https://api.steampowered.com/IEconDOTA2_570/"
   val MATCH_HISTORY_URL = DOTA_BASIC_REST_URL + "GetMatchHistory/V001/?key=%s"
+  val MATCH_HISTORY_URL_START_AT = DOTA_BASIC_REST_URL + "GetMatchHistory/V001/?key=%s&start_at_match_id=%s"
   val MATCH_DETAILS_URL = DOTA_BASIC_REST_URL +"GetMatchDetails/V001/?match_id=%s&key=%s"
   val ITEMS_URL = DOTA_ECON_BASIC_REST_URL + "GetGameItems/V001/?key=%s"
   val HEROES_URL = DOTA_ECON_BASIC_REST_URL + "GetHeroes/V001/?key=%s"
@@ -28,7 +30,15 @@ object RestClient {
 
   def getMatches: Result = {
     val client = ClientBuilder.newBuilder().build()
-    val target = client.target(String.format(MATCH_HISTORY_URL, DOTA_KEY))
+    var matchesUrl: String = ""
+
+    if(StringUtils.isEmpty(latestMatchId)) {
+      matchesUrl = String.format(MATCH_HISTORY_URL, DOTA_KEY)
+    } else {
+      matchesUrl = String.format(MATCH_HISTORY_URL_START_AT, DOTA_KEY, latestMatchId)
+    }
+
+    val target = client.target(matchesUrl)
     val response = target.request().get()
 
     val mapper = new ObjectMapper()
