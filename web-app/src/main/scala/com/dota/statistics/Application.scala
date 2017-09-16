d38cd37d86e3ec80fe2203a94db7d6b3c5ecf6ea
marian.faryna <marian.faryna@vitech.com.ua>
2017-09-16 17:50:07 +0300

    Added dao layer for mongo, initial controller for web-app module

--- web-app/src/main/scala/com/dota/statistics/Application.scala
+++ web-app/src/main/scala/com/dota/statistics/Application.scala
@@ -1,8 +1,11 @@
-package com.dota.statistics.web
+package com.dota.statistics
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+/**
+  * Main application class
+  */
 @SpringBootApplication
 class Application {
 }
