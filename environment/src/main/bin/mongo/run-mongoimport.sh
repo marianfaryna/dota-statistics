4b48b30e00909e3da31a2cd0d49ad8dd476a1eac
4everpunk <ostapshtabura10@gmail.com>
2017-09-29 22:02:45 +0300

    Improve spark scripts & small refactoring

--- environment/src/main/bin/mongo/run-mongoimport.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-command -v mongoimport >/dev/null 2>&1 || { echo >&2 "I require mongoimport but it's not installed.  Aborting."; exit 1; }
-mongoimport --db local --collection test_collection --file test_collection.json
